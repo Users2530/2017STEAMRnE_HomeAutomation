4348eab43a80fed5931d9d682e2d50b3a2303a99
Users <rhkgkrwk001@outlook.com>
2017-12-15 01:45:22 +0000

    Add files via upload

--- Arduino/AllSerialHandling.h
+++ Arduino/AllSerialHandling.h
@@ -8,13 +8,13 @@
 #define ALLSERIALHANDLING_H
 
 //  Sends Data to Pulse Sensor Processing App, Native Mac App, or Third-party Serial Readers.
-void sendDataToSerial(char symbol, int data ){
-    Serial.print(symbol);
-    Serial.println(data);
-  }
-  
-void serialOutput(){   // Decide How To Output Serial.
-  switch(outputType){
+void sendDataToSerial(char symbol, int data ) {
+  Serial.print(symbol);
+  Serial.println(data);
+}
+
+void serialOutput() {  // Decide How To Output Serial.
+  switch (outputType) {
     case PROCESSING_VISUALIZER:
       sendDataToSerial('S', Signal);     // goes to sendDataToSerial function
       break;
@@ -32,11 +32,11 @@ void serialOutput(){   // Decide How To Output Serial.
 }
 
 //  Decides How To OutPut BPM and IBI Data
-void serialOutputWhenBeatHappens(){
-  switch(outputType){
+void serialOutputWhenBeatHappens() {
+  switch (outputType) {
     case PROCESSING_VISUALIZER:    // find it here https://github.com/WorldFamousElectronics/PulseSensor_Amped_Processing_Visualizer
-      sendDataToSerial('B',BPM);   // send heart rate with a 'B' prefix
-      sendDataToSerial('Q',IBI);   // send time between beats with a 'Q' prefix
+      sendDataToSerial('B', BPM);  // send heart rate with a 'B' prefix
+      sendDataToSerial('Q', IBI);  // send time between beats with a 'Q' prefix
       break;
 
     default:
